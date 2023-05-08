667d0c722530328329f9d0239d4cfa2e82416be1
kohaiisweety <130177455+kohaiisweety@users.noreply.github.com>
2023-05-08 14:02:06 +0500

    lab

--- lab7/lab7/Pair.h
+++ lab7/lab7/Pair.h
@@ -1,33 +1,68 @@
 #pragma once
-#include <iostream> 
-using namespace std; 
-class Pair
-{
-public:
-    Pair(void);
-    Pair(int, double);
-    Pair(const Pair&);
-    Pair& operator=(const Pair&);
+#include <iostream>
+#include <string>
+using namespace std;
 
-    //перегруженные операции ввода-вывода
-    friend ostream& operator<< (ostream& out, const Pair&);
-    friend istream& operator>> (istream& in, Pair&);
-
-    //операция умножения на число
-    Pair operator*(const double k) const;
-    //операция сложения
-    Pair operator+(const Pair& p) const;
-    //операция вычитания
-    Pair operator-(const Pair& p) const;
-    //операция умножения
-    Pair operator*(const Pair& p) const;
-    //операция деления
-    Pair operator/(const Pair& p) const;
-    // функция доступа по индексу
-    Pair& operator[](int index);
-public:
-    virtual ~Pair(void) {};
+class Pair {
 private:
-    int first;
-    double second;
+	int first;
+	double second;
+public:
+	Pair() {
+		first = 0;
+		second = 0;
+	};
+	Pair(int F, double S) {
+		first = F;
+		second = S;
+	};
+	Pair(const Pair& t) {
+		first = t.first;
+		second = t.second;
+	};
+	~Pair() {};
+	int get_sirst() { return first; }
+	double get_second() { return second; }
+	void set_first(int F) { first = F; }
+	void set_second(double S) { second = S; }
+	Pair operator -(const Pair& p);
+	Pair& operator +(const int X);
+	Pair& operator +(const double X);
+	Pair operator *(Pair&);
+	friend istream& operator>>(istream& in, Pair& p);
+	friend ostream& operator<<(ostream& out, const Pair& p);
 };
+
+Pair& Pair::operator +(const int X) {
+	first += X;
+	return *this;
+}
+
+Pair& Pair::operator +(const double X) {
+	second += X;
+	return *this;
+}
+
+Pair Pair::operator -(const Pair& p) {
+	Pair temp;
+	temp.first = this->first - p.first;
+	temp.second = this->second - p.second;
+	return temp;
+}
+
+istream& operator >>(istream& in, Pair& t) {
+	cout << "int first = "; in >> t.first;
+	cout << "double second = "; in >> t.second;
+	return in;
+}
+
+ostream& operator <<(ostream& out, const Pair& t) {
+	return (out << t.first << " : " << t.second);
+}
+
+Pair Pair::operator*(Pair& p) {
+	Pair temp;
+	temp.first = (*this).first * p.first;
+	temp.second = (*this).second * p.second;
+	return temp;
+}
--- lab7/lab7/Source.cpp
+++ lab7/lab7/Source.cpp
@@ -1,77 +1,26 @@
-#include <iostream>
+#include "List.h"
 #include "Pair.h"
-#include "Vector.h"
-
-using namespace std;
+#include <Windows.h>
 
 int main() {
-    setlocale(LC_ALL, "ru");
-    // Создаем вектор пар
-    Vector<Pair> pairs(3, Pair(0, 0));
-    int choice = 0;
-    Pair p;
-    Pair p2;
-    while (true) {
-        // Выводим меню
-        cout << "Меню:\n";
-        cout << "1. Добавить элемент в контейнер\n";
-        cout << "2. Добавить элемент 2 в контейнер\n";
-        cout << "3. Умножение\n";
-        cout << "4. Деление\n";
-        cout << "5. Сложение\n";
-        cout << "6. Вычитание\n";
-        cout << "7. Введите индекс чтобы получить элемент\n";
-        cout << "8. Выход\n";
-        cout << "Введите ваш выбор: ";
-        cout << endl;
-        cin >> choice;
-        switch (choice) {
-        case 1:
-            // Добавляем новую пару в вектор
-            cin >> p;
-            pairs = pairs + p;
-            break;
-        case 2:
-            // Вводим вторую пару
-            cout << "Введите значение для второй пары:\n";
-            cin >> p2;
-            pairs = pairs + p2;
-            break;
-        case 3:
-            // Умножаем пары и выводим результат
-            cout << "p * p2 = " << p*p2 << endl;
-            break;
-        case 4:
-            // Делим пары и выводим результат
-            cout << "p / p2 = " << p / p2 << endl;
-            break;
-        case 5:
-            // Складываем пары и выводим результат
-            cout << "p + p2 = " << p + p2 << endl;
-            break;
-        case 6:
-            // Вычитаем пары и выводим результат
-            cout << "p - p2 = " << p - p2 << endl;
-            break;
-        case 7:
-            // Получаем элемент по индексу и выводим результат
-            int index;
-            cout << "Введите индекс элемента: ";
-            cin >> index;
-            if (index >= 0 && index < pairs()) {
-                cout << "Элемент[" << index << "]: " << pairs[index] << endl;
-            }
-            else {
-                cout << "Неверный индекс элемента.\n";
-            }
-            break;
-        case 8:
-            return 0;
-        default:
-            cout << "Неверный выбор. Попробуйте еще раз.\n";
-            break;
-        }
-    }
-
-    return 0;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Pair p1(2, 2.5);
+	List<Pair> a(3, p1);
+	cout << a << endl;
+	cout << "Первый элемент: " << a.front() << endl;
+	cin >> a;
+	cout << a << endl;
+	List<Pair> b(a);
+	cout << "Список был скопирован\n";
+	cout << b << endl;
+	List<Pair> c = a * b;
+	cout << "a * b\n" << c << endl;
+	Pair num;
+	cout << "Введите новое значение:\n"; cin >> num;
+	b.pushback(num);
+	b.popfront();
+	for (Iterator<Pair> iter = b.first(); iter != b.last(); ++iter)
+		cout << *iter << '\n';
+	return 0;
 }
